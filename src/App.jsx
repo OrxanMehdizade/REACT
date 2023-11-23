7f8a771bbf797aee4deb3a5f937f3163a1e2c298
Mehdi_ha01 <mehdizadeorxan2000@gmail.com>
2023-11-23 13:59:34 +0400

    lesson 7 context

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,45 @@
-//import logo from './logo.svg';
+
 import './App.css';
-import {Routes,Route} from "react-router-dom";
-import HeaderComponent from "./HeaderComponent";
-import Home from './Home'
-import About from './About'
+import FormComponent from './FormComponent'
+import {createContext,useState} from "react";
+import {goodsArray} from "./reducer";
+
+export const MyContext=createContext();
+
+
 
 function App() {
+    let [arr,setArr]=useState(goodsArray);
+    let [obj,setObj]=useState({"product_name": "",
+        "product_description": "", "product_price": null})
 
+    const addGoodus=()=>{
+        let newArr=[...arr]
+        newArr.push(obj)
+        setArr(newArr)
+    }
+
+    const deleteGoodus=(goodsName)=>{
+        let newArr=[...arr]
+        let arrFilter=newArr.filter((item)=>item.product_name!==goodsName)
+        setArr(arrFilter)
+    }
+
+    const updateGoods=(goodsName)=>{
+        let newArr = [...arr];
+        let arrMap = newArr.map((item) =>
+            item.product_name === goodsName ? obj : item
+        );
+        setArr(arrMap);
+
+    }
 
     return (
-        <div className='countenirDiv'>
-            <HeaderComponent/>
-            <Routes>
-                <Route path='/home/*' element={<Home />} ></Route>
-                <Route path='/About' element={<About />} ></Route>
-            </Routes>
-
-        </div>
+        <MyContext.Provider value={{arr,setObj,obj,addGoodus,deleteGoodus,updateGoods}}>
+            <div className='countenirDiv'>
+                <FormComponent/>
+            </div>
+        </MyContext.Provider>
     );
 }
 
